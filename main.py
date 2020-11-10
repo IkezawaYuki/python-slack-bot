8963cf54673531090cbe826157d1cbad8e88d8b6
yikezawa <y.ikezawa93@gmail.com>
2020-11-10 22:52:30 +0900

    mo

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from slackbot.bot import Bot, respond_to
+from slackbot.bot import Bot, respond_to, listen_to
+from datetime import datetime
+import openpyxl
 
 
 @respond_to('練習')
 def sample(message):
     message.send("これは練習です。")
+
+
+@listen_to('れんしゅう')
+def sample2(message):
+    message.send("これは「れんしゅう」です。")
+
+
+@listen_to('trial')
+def sample3(message):
+    message.reply("trial")
+
+
+@respond_to("出勤")
+def punch_in(message):
+    timestamp = datetime.now().strftime("%H:%M")
+    message.send(f'出勤時刻は{timestamp}です。')
+
+
+@respond_to("退勤")
+def punch_out(message):
+    timestamp = datetime.now().strftime("%H:%M")
+    message.send(f'退勤時刻は{timestamp}です。')
 
 
 if __name__ == '__main__':
